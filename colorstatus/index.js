252444bf33e26d089443badba9261b418bee79e6
student-college-beginner <elsa.parra.quito@gmail.com>
2026-02-11 12:06:43 +0800

    Create index.js file with Express and Socket.io setup

---
+++ colorstatus/index.js
@@ -0,0 +1,25 @@
+// index.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from Express and Socket.io!');
+});
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
